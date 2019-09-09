3afa7a8611cbc2a67ebf22d3061ed611e4921a58
Max Hutchinson <maxhutch@gmail.com>
2019-09-09 14:30:00 -0400

    Update scripts/strip_hints.sh
    Co-Authored-By: bfolie <bfolie@citrine.io>

--- scripts/strip_hints.sh
+++ scripts/strip_hints.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Removing type hints to satisfy Python 3.5"
-find ./src -regex '.*\.py'|while read fileName; do
+find ./taurus -regex '.*\.py'|while read fileName; do
   echo $fileName
   strip-hints $fileName --to-empty --only-assigns-and-defs >tmp.py
   mv tmp.py $fileName
