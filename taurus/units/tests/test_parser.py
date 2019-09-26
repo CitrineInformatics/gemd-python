025114e7c8906c450d91e3c4fac64a6844eb1ae4
Brendan Folie <bfolie@citrine.io>
2019-09-26 16:11:08 -0700

    Write tests of units

--- taurus/units/tests/test_parser.py
+++ taurus/units/tests/test_parser.py
@@ -1,5 +1,10 @@
 import pytest
+import pkg_resources
+from pint import UnitRegistry
 from taurus.units import parse_units, UndefinedUnitError
+
+# use the default unit registry for now
+_ureg = UnitRegistry(filename=pkg_resources.resource_filename("taurus.units", "citrine_en.txt"))
 
 
 def test_parse_expected():
@@ -10,7 +15,8 @@ def test_parse_expected():
         "L", "mL",
         "inch", "ft", "mm", "um",
         "second", "ms", "hour", "minute", "ns",
-        "g/cm^3", "g/mL", "kg/cm^3"
+        "g/cm^3", "g/mL", "kg/cm^3",
+        _ureg("kg").u
     ]
     for unit in expected:
         parse_units(unit)
@@ -23,8 +29,14 @@ def test_parse_unexpected():
         "slug",
         "hand",
         "year",
-        "St"
+        "St",
+        5
     ]
     for unit in unexpected:
         with pytest.raises(UndefinedUnitError):
             parse_units(unit)
+
+
+def test_parse_none():
+    """Test that None parses as None."""
+    assert parse_units(None) is None
