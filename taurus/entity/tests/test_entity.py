656efb246aab35af6a33e3db110a6cc7010db0be
Brendan Folie <bfolie@citrine.io>
2019-09-26 14:11:02 -0700

    Remove content_hash

--- taurus/entity/tests/test_entity.py
+++ taurus/entity/tests/test_entity.py
@@ -1,11 +1,7 @@
 """General tests of entities."""
 import pytest
-from uuid import uuid4
 
-from taurus.entity.attribute.condition import Condition
 from taurus.entity.object.ingredient_run import IngredientRun
-from taurus.entity.object.process_run import ProcessRun
-from taurus.entity.value.nominal_real import NominalReal
 
 
 def test_id_case_sensitivitiy():
@@ -16,17 +12,3 @@ def test_id_case_sensitivitiy():
     ingredient = IngredientRun(uids={'my_id': 'sample1'})
     assert ingredient.uids['my_id'] == 'sample1'
     assert ingredient.uids['MY_id'] == 'sample1'
-
-
-def test_content_hash():
-    """Test that the content has is a string with all expected fields."""
-    proc = ProcessRun("a process", uids={'id': str(uuid4())},
-                      conditions=Condition("something", value=NominalReal(-10, '')))
-    ingred = IngredientRun(name="ingredient", uids={'id': str(uuid4())},
-                           absolute_quantity=NominalReal(3.5, 'g'), process=proc)
-    ingred_hash = ingred.content_hash()
-    assert isinstance(ingred_hash, str)
-    assert 'absolute_quantity' in ingred_hash \
-           and 'name' in ingred_hash \
-           and 'uids' in ingred_hash \
-           and 'process' in ingred_hash
