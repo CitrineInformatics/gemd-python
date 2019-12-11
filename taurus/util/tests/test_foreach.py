55fd8256106ef4a00d1ac01e699399cb46e18fdf
Max Hutchinson <maxhutch@gmail.com>
2019-12-11 10:11:12 -0500

    Appease the flaky gods

--- taurus/util/tests/test_foreach.py
+++ taurus/util/tests/test_foreach.py
@@ -3,10 +3,10 @@ from taurus.util.impl import recursive_foreach
 
 
 def test_recursive_foreach():
-    """Test that recursive foreach will actually walk through a material history"""
+    """Test that recursive foreach will actually walk through a material history."""
     mat_run = MaterialRun("foo")
     process_run = ProcessRun("bar")
-    ingredient_run = IngredientRun(process=process_run, material=mat_run, name="foobar")
+    IngredientRun(process=process_run, material=mat_run, name="foobar")
     output = MaterialRun(process=process_run)
 
     types = []
