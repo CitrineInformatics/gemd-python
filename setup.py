3d17871f464a61aff965d2c9b79b1a3fd7a57b1b
Max Hutchinson <maxhutch@gmail.com>
2020-02-10 17:50:50 -0500

    Adding comment on setup.py block

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(name='taurus-citrine',
           "pint>=0.9",
           "strip-hints>=0.1.5"
       ],
+      # TODO: add this back when we apply them on deployments
       # cmdclass={
       #     'install': PostInstallCommand,
       #     'develop': PostDevelopCommand
