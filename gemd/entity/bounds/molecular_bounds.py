adf5160249172eaa17a146b24ecdbc29b878b38a
Ken Kroenlein <kkroenlein@citrine.io>
2020-05-01 13:17:32 -0600

    Added molecular bounds

---
+++ gemd/entity/bounds/molecular_bounds.py
@@ -0,0 +1,52 @@
+"""
+Bounds a molecular structure to be a valid representation.
+
+In the future, this may include substructural restrictions.
+"""
+from gemd.entity.bounds.base_bounds import BaseBounds
+
+
+class MolecularBounds(BaseBounds):
+    """Molecular bounds, with no component or substructural restrictions (yet)."""
+
+    typ = "molecular_bounds"
+
+    def __init__(self):
+        pass
+
+    def contains(self, bounds: BaseBounds) -> bool:
+        """
+        Check if another bounds is contained by this bounds.
+
+        The other bounds must also be a MolecularBounds.  There are no other
+        conditions at this time.
+
+        Parameters
+        ----------
+        bounds: BaseBounds
+            Other bounds object to check.
+
+        Returns
+        -------
+        bool
+            True if the other bounds is contained by this bounds.
+
+        """
+        if not super().contains(bounds):
+            return False
+        if not isinstance(bounds, MolecularBounds):
+            return False
+
+        return True
+
+    def as_dict(self):
+        """
+        Convert bounds to a dictionary.
+
+        Returns
+        -------
+        dict
+            A dictionary with "type".
+
+        """
+        return {"type": self.typ}
