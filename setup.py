8a64032f5bd9d134bee828abb1bb4f1b5f93a4a9
Ventura Rivera <vrivera@citrine.io>
2020-05-07 20:48:22 -0700

    version bump

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class PostDevelopCommand(develop):
 
 
 setup(name='gemd',
-      version='0.7.3',
+      version='0.8.3',
       url='http://github.com/CitrineInformatics/gemd-python',
       description="Python binding for Citrine's GEMD data model",
       author='Max Hutchinson',
