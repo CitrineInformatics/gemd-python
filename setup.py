33ceea0ea17c51324d50ca43777c69469ed8889d
Ken Kroenlein <kkroenlein@citrine.io>
2020-07-15 18:41:11 -0600

    Minor version bump

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class PostDevelopCommand(develop):
 
 
 setup(name='gemd',
-      version='0.10.0',
+      version='0.12.0',
       url='http://github.com/CitrineInformatics/gemd-python',
       description="Python binding for Citrine's GEMD data model",
       author='Max Hutchinson',
