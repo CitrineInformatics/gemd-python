2abe60e9db5e09ca578d8b43322793f812af2a63
Ken Kroenlein <kkroenlein@citrine.io>
2020-09-11 12:20:11 -0600

    doc fix

--- gemd/entity/case_insensitive_dict.py
+++ gemd/entity/case_insensitive_dict.py
@@ -108,8 +108,6 @@ class CaseInsensitiveDict(dict):
         """
         Remove and return a (key, value) pair from the dictionary.
 
-        Pairs are returned in LIFO order.
-
         popitem() is useful to destructively iterate over a dictionary, as often used
         in set algorithms.  If the dictionary is empty, calling popitem() raises a
         KeyError.
