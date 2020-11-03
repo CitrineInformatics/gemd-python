f0b86ef0a5c61886acf3aec44b5f46d1552356ae
Ken Kroenlein <kkroenlein@citrine.io>
2020-11-03 11:23:33 -0700

    Travis test commit

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class PostDevelopCommand(develop):
 
 
 setup(name='gemd',
-      version='0.16.1',
+      version='0.16.2',
       url='http://github.com/CitrineInformatics/gemd-python',
       description="Python binding for Citrine's GEMD data model",
       author='Max Hutchinson',
