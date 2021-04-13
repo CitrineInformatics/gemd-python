313342ce01287e83f594e8b41b785472547e1099
Ken Kroenlein <kkroenlein@citrine.io>
2021-04-13 15:25:13 -0600

    Update required pint, as per PLA-7139

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='gemd',
-      version='0.17.3',
+      version='0.17.4',
       url='http://github.com/CitrineInformatics/gemd-python',
       description="Python binding for Citrine's GEMD data model",
       author='Max Hutchinson',
@@ -21,7 +21,7 @@ setup(name='gemd',
       install_requires=[
           "toolz",
           "pytest>=4.3",
-          "pint>=0.9",
+          "pint>=0.10",
           "deprecation>=2.0.7,<3"
       ],
       classifiers=[
