536f34d545d0b7abf7424e0fa09fe93544bf5ef5
Brendan Folie <bfolie@citrine.io>
2021-06-14 15:54:41 -0700

    copy docstrings into attributes

--- gemd/entity/attribute/parameter.py
+++ gemd/entity/attribute/parameter.py
@@ -9,6 +9,24 @@ class Parameter(BaseAttribute):
     affect a process or measurement: e.g. oven dial temperature for a kiln firing, magnification
     for a measurement taken with an electron microscope.
 
+    Parameters
+    ----------
+    name: str
+        Required name of the attribute. Each attribute within an object must have a unique name.
+    notes: str
+        Optional free-form notes about the attribute.
+    value: :py:class:`BaseValue <gemd.entity.value.base_value.BaseValue>`
+        The value of the attribute.
+    template: :class:`AttributeTemplate \
+    <gemd.entity.template.attribute_template.AttributeTemplate>`
+        Attribute template that defines the allowed bounds of this attribute. If a template
+        and value are both present then the value must be within the template bounds.
+    origin: str
+        The origin of the attribute. Must be one of "measured", "predicted", "summary",
+        "specified", "computed", or "unknown." Default is "unknown."
+    file_links: List[FileLink]
+        Links to files associated with the attribute.
+
     """
 
     typ = "parameter"
