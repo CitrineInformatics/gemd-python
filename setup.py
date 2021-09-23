40defda8901e1c0e1687f59273933ada609f7bba
Ken Kroenlein <kkroenlein@citrine.io>
2021-09-23 09:24:46 -0600

    Migrate arguments for from_entity

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='gemd',
-      version='1.0.2',
+      version='1.1.0',
       url='http://github.com/CitrineInformatics/gemd-python',
       description="Python binding for Citrine's GEMD data model",
       author='Max Hutchinson',
