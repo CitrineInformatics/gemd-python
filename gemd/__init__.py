d527be68099092b024434882f4f8b5fe09e62342
Ken Kroenlein <kkroenlein@citrine.io>
2021-12-23 12:30:52 -0700

    Simplify import interface

--- gemd/__init__.py
+++ gemd/__init__.py
@@ -1,1 +1,15 @@
 """Data concepts library."""
+# flake8: noqa
+from .entity import Condition, Parameter, Property, PropertyAndConditions, \
+    CategoricalBounds, CompositionBounds, IntegerBounds, \
+    MolecularStructureBounds, RealBounds, \
+    MaterialRun, MeasurementRun, ProcessRun, IngredientRun, \
+    MaterialSpec, MeasurementSpec, ProcessSpec, IngredientSpec, \
+    PerformedSource, \
+    PropertyTemplate, ConditionTemplate, ParameterTemplate, \
+    MaterialTemplate, MeasurementTemplate, ProcessTemplate, \
+    NominalReal, NormalReal, UniformReal, NominalInteger, \
+    UniformInteger, DiscreteCategorical, NominalCategorical, \
+    EmpiricalFormula, NominalComposition, InChI, Smiles, \
+    LinkByUID, \
+    FileLink
---
+++ gemd/entity/source/__init__.py
@@ -0,0 +1,2 @@
+# flake8: noqa
+from .performed_source import PerformedSource
