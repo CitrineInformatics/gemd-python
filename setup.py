afb9463cafe62e93caff1345e9b3a2918b90a03f
Ken Kroenlein <kkroenlein@citrine.io>
2022-08-17 14:09:08 -0600

    Bypass conversion computation when units strings match

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='gemd',
-      version='1.10.2',
+      version='1.10.3',
       url='http://github.com/CitrineInformatics/gemd-python',
       description="Python binding for Citrine's GEMD data model",
       author='Citrine Informatics',
